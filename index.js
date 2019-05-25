cbb5d29839cacf25d9397b059bff1f4005746706
Joshua Persaud <joshuapersaud2@gmail.com>
2019-05-25 18:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays()
   return "Happy holidays!"
   }
 
-function happyHolidaysTo(name) {
-  return `Happy Holidays, ${name}!`
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidaysTo(holiday, name) {
